42dcff1688ae1cc4731245727012a51a797b42e1
Charles Hartmann <cohartma@ucsc.edu>
2024-01-24 15:26:35 -0800

    Super basic profile page template started

--- JamCircle/frontend/src/components/HomePage.js
+++ JamCircle/frontend/src/components/HomePage.js
@@ -7,6 +7,13 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material/Typography";
+import { TextField } from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Radio from "@mui/material/Radio";
 
 export default class HomePage extends Component {
   constructor(props) {
