a8f5b4555a4c2879ce8442578786d4672a46204e
Charles Hartmann <cohartma@ucsc.edu>
2024-01-28 11:28:22 -0800

    Brandon spotifyAPI

--- JamCircle/spotifyAPI/auth.py
+++ JamCircle/spotifyAPI/auth.py
@@ -1,4 +1,4 @@
 CLIENT_ID = "f9b6e983025b430898b337cf9f5d73a3"
 CLIENT_SECRET = "65204a5248404cc39f610690b1f32279"
-REDIRECT_URI = ""
-SPOTIFY_URL = 'https://api.spotify.com/v1/me/'
+REDIRECT_URI = "http://127.0.0.1:8000/auth/redirect"
+SPOTIFY_URL = "https://api.spotify.com/v1"
--- JamCircle/spotifyAPI/urls.py
+++ JamCircle/spotifyAPI/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import AuthURL, spotfy_callback, Authenticated
+from .views import SpotifyLogin, spotfy_callback, Authenticated
 urlpatterns = [
-    path('authSpotify', AuthURL.as_view()),
+    path('authSpotify', SpotifyLogin.as_view()),
     path('redirect', spotfy_callback),
-    path('is-authenticated', Authenticated)
+    path('is-authenticated', Authenticated.as_view())
 ]
