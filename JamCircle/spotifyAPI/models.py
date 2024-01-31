3d3eb520452d9c0deafa2990d110e1116b8ad325
Namebrand71 <brandontlawrence@gmail.com>
2024-01-31 12:40:49 -0800

    migrated

--- JamCircle/spotifyAPI/models.py
+++ JamCircle/spotifyAPI/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class SpotifyToken(models.Model):
-    session_id = models.CharField(max_length=100, unique=True)
-    user_id = models.CharField(max_length=100, unique=True)
+    session_id = models.CharField(max_length=100, unique=True, default="foobar")
+    user_id = models.CharField(max_length=100, unique=True, default="foobar")
     create_at = models.DateTimeField(auto_now_add=True)
-    access_token = models.CharField(max_length=200)
-    refresh_token = models.CharField(max_length=200)
-    expires_at = models.DateTimeField()
-    token_type = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=200, default="foobar")
+    refresh_token = models.CharField(max_length=20, default="foobar")
+    expires_at = models.DateTimeField(auto_now_add=True)
+    token_type = models.CharField(max_length=50, default="foobar")
