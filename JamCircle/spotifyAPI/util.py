7ba77fb4d855602a0f9c2479755e569f1d82f2cb
Charles Hartmann <cohartma@ucsc.edu>
2024-01-30 17:39:41 -0800

    Refactored code to use util.py functions. Also fixed code in util.py

--- JamCircle/spotifyAPI/util.py
+++ JamCircle/spotifyAPI/util.py
@@ -4,10 +4,15 @@ from datetime import timedelta
 from requests import Request, post, put, get
 from .auth import CLIENT_ID, CLIENT_SECRET, SPOTIFY_URL
 
+
 def is_authenticated(session_id):
     token = get_user_token(session_id)
+    print("is_authenticated called")
+    print("Expires at: ", token.expires_at)
+    print("Time now: ", timezone.now())
     if token:
-        if token.expires_in < timezone.now():
+        if token.expires_at < timezone.now():
+            print("token has expired, getting a new token now")
             refresh_token(session_id)
         return True
     return False
@@ -23,10 +28,11 @@ def refresh_token(session_id):
     }).json()
 
     access_token = response.get('access_token')
+
     refresh_token = response.get('refresh_token')
-    expires_in = response.get('expires_in')
     token_type = response.get('token_type')
-    user_token_func(session_id, access_token, token_type, expires_in, refresh_token)
+    user_token_func(session_id, access_token, token_type, refresh_token)
+
 
 def get_user_token(session_id):
     user_tokens = SpotifyToken.objects.filter(user=session_id)
@@ -34,55 +40,66 @@ def get_user_token(session_id):
         return user_tokens[0]
     else:
         return None
-    
-def user_token_func(session_id, access_token, token_type, expires_in, refresh_token):
+
+
+def user_token_func(session_id, access_token, token_type, expires_at, refresh_token):
     tokens = get_user_token(session_id)
-    expires_in = timezone.now() + timedelta(seconds=expires_in)
+    if not expires_at:
+        expires_at = timezone.now() + timedelta(seconds=3600)
 
     if tokens:
         tokens.access_token = access_token
-        tokens.refresh_token = refresh_token
-        tokens.expires_in = expires_in
-        tokens.token_type = token_type
-        tokens.save(update_fields=['access_token','refresh_token','expires_in'])
+        tokens.expires_at = expires_at
+        tokens.token_type = token_type  # Unsure if this needs to be updated
+        tokens.save(update_fields=['access_token', 'expires_at'])
     else:
-        tokens = SpotifyToken(user=session_id, access_token=access_token, refresh_token=refresh_token, expires_in=expires_in,token_type=token_type)
+        tokens = SpotifyToken(user=session_id, access_token=access_token,
+                              refresh_token=refresh_token, expires_at=expires_at, token_type=token_type)
         tokens.save()
 
-def spotify_api_request(session_id, endpoint, ifPost=False,ifPut=False):
+
+def spotify_api_request(session_id, endpoint, ifPost=False, ifPut=False):
     token = get_user_token(session_id)
-    header = {'Content-Type': 'application/json', 'Authorization': "Bearer " + token.access_token}
+    header = {'Content-Type': 'application/json',
+              'Authorization': "Bearer " + token.access_token}
 
     if ifPost:
         post(SPOTIFY_URL + endpoint, headers=header)
     if ifPut:
         put(SPOTIFY_URL + endpoint, headers=header)
-    responce = get(SPOTIFY_URL + endpoint, headers=header)
-    #Might not get something back
-    return responce.json
+    response = get(SPOTIFY_URL + endpoint, headers=header)
+    # Might not get something back
+    return response.json()
+
 
 def getTop10Artist(session_id):
-    responce = spotify_api_request(session_id,'/me/top/artists?time_range=long&limit=10&offset=0', False, False)
-    art_list = responce.get('items')
+    response = spotify_api_request(
+        session_id, '/me/top/artists?time_range=long&limit=10&offset=0', False, False)
+    art_list = response.get('items')
     return art_list.json
 
+
 def getTop10Tracks(session_id):
-    responce = spotify_api_request(session_id,'/me/top/tracks?time_range=long&limit=10&offset=0', False, False)
-    track_list = responce.get('items')
+    response = spotify_api_request(
+        session_id, '/me/top/tracks?time_range=long&limit=10&offset=0', False, False)
+    track_list = response.get('items')
     return track_list.json
 
-#Give a list of strings of Spotify IDs. Give session_id and list of Spotify IDs
+# Give a list of strings of Spotify IDs. Give session_id and list of Spotify IDs
+
+
 def checkFollowing(session_id, list):
     for x in list:
         idString = idString + "%2C" + x
-    #Removes first %2C
+    # Removes first %2C
     idString = idString[3:]
 
     requestEndpoint = '/me/following/contains?type=user&ids=' + idString
-        
-    responce = spotify_api_request(session_id,requestEndpoint, False, False)
-    return responce.json
+
+    response = spotify_api_request(session_id, requestEndpoint, False, False)
+    return response.json
+
 
 def getUserJSON(session_id):
-    responce = spotify_api_request(session_id, "/me", False, False)
-    return responce.json
+    response = spotify_api_request(session_id, "/me", False, False)
+    return response
--- JamCircle/spotifyAPI/views.py
+++ JamCircle/spotifyAPI/views.py
@@ -5,7 +5,7 @@ from requests import Request, post
 import requests
 from rest_framework import status
 from rest_framework.response import Response
-from .util import user_token_func, is_authenticated, refresh_token, get_user_token
+from .util import user_token_func, is_authenticated, refresh_token, get_user_token, getUserJSON
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta, datetime
@@ -42,16 +42,14 @@ def spotfy_callback(request, format=None):
     access_token = response.get('access_token')
     token_type = response.get('token_type')
     refresh_token = response.get('refresh_token')
-    expires_in = response.get('expires_in')
     error = response.get('error')
 
     if not request.session.exists(request.session.session_key):
         request.session.create()
 
+    expires_at = timezone.now() + timedelta(hours=1)
     user_token_func(request.session.session_key, access_token,
-                    token_type, expires_in, refresh_token)
-
-    expires_in = timezone.now() + timedelta(seconds=response.get('expires_in'))
+                    token_type, expires_at, refresh_token)
 
     # Create/Update spotify token in db
     SpotifyToken.objects.update_or_create(
@@ -60,7 +58,7 @@ def spotfy_callback(request, format=None):
             'access_token': access_token,
             'refresh_token': refresh_token,
             'token_type': token_type,
-            'expires_in': expires_in
+            'expires_at': expires_at
         }
     )
 
@@ -81,60 +79,12 @@ class GetSpotifyProfile(APIView):
         session_key = request.session.session_key
         print("Session key: ", session_key)
         token = SpotifyToken.objects.filter(user=session_key).first()
-        print("Token: ", token)
-        # TODO: Check if token has expired, if so need to use refresh token to generate new access token
-        if token:
-            now = datetime.now(pytz.utc)
 
-            print("Time now: ", now)
-            print("Expires at: ", token.expires_at)
-            if now >= token.expires_at:  # Token has expired, need to generate a new access token
-                new_access_token, new_expires_at = self.refresh_access_token(
-                    token.refresh_token)
-                token.access_token = new_access_token
-                token.expires_at = new_expires_at
-                token.save()
-            headers = {
-                'Authorization': f'Bearer {token.access_token}'
-            }
-            profile_response = requests.get(
-                'https://api.spotify.com/v1/me', headers=headers)
-            if profile_response.status_code == 200:
-                return Response(profile_response.json(), status=profile_response.status_code)
+        if is_authenticated(session_key):
+            profile_response = getUserJSON(session_key)
+            if profile_response:
+                return Response(profile_response, status=200)
             print("Failed to fetch spotify profile")
-            return Response({'error': 'Failed to fetch Spotify profile'}, status=profile_response.status_code)
-        return Response({'error': 'No Spotify token found'}, status=400)
-
-    def refresh_access_token(self, refresh_token):
-        token_url = 'https://accounts.spotify.com/api/token'
-
-        # Client credentials
-        client_id = CLIENT_ID
-        client_secret = CLIENT_SECRET
-
-        client_creds = f"{client_id}:{client_secret}"
-        client_creds_b64 = b64encode(client_creds.encode()).decode()
-
-        headers = {
-            'Authorization': f'Basic {client_creds_b64}',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        data = {
-            'grant_type': 'refresh_token',
-            'refresh_token': refresh_token
-        }
-
-        response = requests.post(token_url, headers=headers, data=data)
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            response_data = response.json()
-            new_access_token = response_data['access_token']
-
-            expires_in = response_data['expires_in']
-            now = datetime.now(pytz.utc)
-            new_expires_at = now + timedelta(seconds=expires_in)
-
-            return new_access_token, new_expires_at
+            return Response({'error': 'Failed to fetch Spotify profile'}, status=400)
         else:
-            raise Exception("Failed to refresh Spotify token")
+            return Response({'error': 'Authentication with Spotify failed or no token found'}, status=403)
