1a06c1d3f5c3a10f624777eda8d10980d367c2e7
Charles Hartmann <cohartma@ucsc.edu>
2024-02-02 17:28:56 -0800

    Fixed React error and updated gitignore to avoid license

--- JamCircle/frontend/src/components/App.js
+++ JamCircle/frontend/src/components/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return <HomePage />;
-    }
+  render() {
+    return <HomePage />;
+  }
 }
 
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
