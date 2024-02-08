9cb1ec9a9c0779c2da2cc84a0d1622e4c8abc54a
Namebrand71 <46506313+Namebrand71@users.noreply.github.com>
2024-02-07 19:43:30 -0800

    Add files via upload

---
+++ JamCircle/frontend/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index
+from reviews.views import song_page
+
+app_name = 'frontend'
+urlpatterns = [
+    path('', index, name='home'),
+    path('profile', index, name='profile'),
+    path('song/<str:spotify_content_id>', song_page, name='song')
+]
---
+++ JamCircle/frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/, // Add this test for CSS files
+        use: ["style-loader", "css-loader"], // Use these loaders for CSS files
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("development"), //NOTE: HARDCODED development here to get `npm run dev` to work
+      },
+    }),
+  ],
+};
