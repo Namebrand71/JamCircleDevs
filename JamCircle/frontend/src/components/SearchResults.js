c84b895ad73efe6b368e740f2b973e97fc200902
Charles Hartmann <cohartma@ucsc.edu>
2024-02-09 17:54:15 -0800

    Still an issue with the navbar display

--- JamCircle/frontend/src/components/SearchResults.js
+++ JamCircle/frontend/src/components/SearchResults.js
@@ -4,6 +4,27 @@ import Navbar from "./NavBar";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 
+const ReturnedResults = ({ results, search_query }) => {
+  return (
+    <Grid item xs={6}>
+      <h2>Search Results for "{decodeURIComponent(search_query)}"</h2>
+      <ul>
+        {results.map((track, index) => (
+          <li key={index}>
+            <Link
+              to={`/song/${track.id}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              {track.name} by{" "}
+              {track.artists.map((artist) => artist.name).join(", ")}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Grid>
+  );
+};
+
 const SearchResults = () => {
   const { search_query } = useParams();
   const [results, setResults] = useState([]);
@@ -25,30 +46,18 @@ const SearchResults = () => {
     setLoading(false);
   };
 
-  if (loading) return <div>Loading...</div>;
-
   return (
     <div className="searchpage">
       <Grid container spacing={1} alignItems={"flex-start"}>
         <Grid item xs={12} align="right">
           <Navbar />
         </Grid>
-        <Grid item xs={6}>
-          <h2>Search Results for "{decodeURIComponent(search_query)}"</h2>
-          <ul>
-            {results.map((track, index) => (
-              <li key={index}>
-                <Link
-                  to={`/song/${track.id}`}
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  {track.name} by{" "}
-                  {track.artists.map((artist) => artist.name).join(", ")}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </Grid>
+
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ReturnedResults results={results} search_query={search_query} />
+        )}
       </Grid>
     </div>
   );
