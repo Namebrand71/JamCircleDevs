629007e7bcaeeccaa358597b86d6610869447f81
Namebrand71 <brandontlawrence@gmail.com>
2024-02-19 15:44:42 -0800

    about to merge

--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
-from .models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from spotifyAPI.util import *
+from .models import User, Friend_Request
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -56,3 +57,51 @@ def getUserFromSession(session_id):
     token = get_user_token(session_id)
     current_user = User.objects.filter(token=token).first()
     return current_user
+
+def send_friend_request(request, to_user_id):
+    from_user = getUserFromSession(request.session.session_key)
+    to_user = User.objects.get(user_id=to_user_id)
+    friend_request, exist = Friend_Request.objects.get_or_create(from_user=from_user, to_user=to_user)
+
+
+    if exist:
+        return HttpResponse('Friend Request Already Sent')
+    else:
+        to_user.pending_friend_requests.add(friend_request)
+        from_user.pending_friend_requests.add(friend_request)
+        return HttpResponse('Friend Request Made!')
+
+def accept_friend_request(request, spotify_id):
+    to_user = getUserFromSession(request.session.session_key)
+    from_user = User.objects.filter(spotify_id=spotify_id).first()
+    friend_request, exists = to_user.pending_friend_requests.get(from_user=from_user)
+    if exists:
+        to_user.friends.add(from_user)
+        from_user.friends.add(to_user)
+        to_user.pending_friend_requests.delete(from_user=from_user)
+        from_user.pending_friend_requests.delete(to_user=to_user)
+        HttpResponse('Friend Request Accepted!')
+    else:
+        HttpResponse('error accepting friend request')
+
+def reject_friend_request(request, from_user_id):
+    to_user = getUserFromSession(request.session.session_key)
+    from_user = User.objects.get(from_user_id)
+    friend_request, exists = to_user.pending_friend_requests.get(from_user=from_user)
+    if exists:
+        to_user.pending_friend_requests.delete(from_user=from_user)
+        from_user.pending_friend_requests.delete(to_user=to_user)
+        HttpResponse('Friend Request Rejected!')
+    else:
+        HttpResponse('error rejectinging friend request')
+
+def cancel_friend_request(request, to_user_id):
+    to_user = User.objects.get(to_user_id)
+    from_user = getUserFromSession(request.session.session_key)
+    friend_request, exists = from_user.pending_friend_requests.get(to_user=to_user)
+    if exists:
+        to_user.pending_friend_requests.delete(from_user=from_user)
+        from_user.pending_friend_requests.delete(to_user=to_user)
+        HttpResponse('Friend Request Canceled!')
+    else:
+        HttpResponse('error canceling friend request')
