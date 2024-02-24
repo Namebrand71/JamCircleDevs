0989a2d4841c11c353381143cbac0a00b3b99270
Charles Hartmann <cohartma@ucsc.edu>
2024-02-24 13:27:29 -0800

    Improved player styling

--- frontend/src/components/PlayerWrapper.js
+++ frontend/src/components/PlayerWrapper.js
@@ -15,13 +15,17 @@ const PlayerWrapper = ({
   if (!spotifyContentId) {
     return null;
   }
+
   return (
-    <SpotifyPlayer
-      // token="BQAkfdsdaSQvBksGcmDvlkj2DRPKHn-G1PvnM67Tw4ILAeZvtKSNrs4iTO0_dXC7nAmOZlW-wU6FTmAlxr688g1ss9B3cXniiUcS_B36dyCJiQEMbNiuLQ_BlV12oislGTBwIZXYhR1gP1aF5pYmU3FfEeUulRwYGX635mOd0Qr7P_aA31kQzLbZDU1qZuaDYkUNdQBZyXNfztnZdPyLmBME8QzjpCcbcJ4gdA"
-      token={accessToken}
-      uris={[`spotify:${spotifyContentType}:${spotifyContentId}`]}
-      play={true}
-    />
+    <>
+      <div style={{ marginTop: "auto" }}></div>
+      <SpotifyPlayer
+        token={accessToken}
+        uris={[`spotify:${spotifyContentType}:${spotifyContentId}`]}
+        play={true}
+        layout="compact"
+      />
+    </>
   );
 };
 
