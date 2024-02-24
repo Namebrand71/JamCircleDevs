16dd20097ad7fc798f4ba4af94457873596dcdcd
Charles Hartmann <cohartma@ucsc.edu>
2024-02-23 19:43:17 -0800

    Basic Listening history on homepage

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, all_listening_history
 
 urlpatterns = [
-    path('profile', ProfileView.as_view())
+    path('profile', ProfileView.as_view()),
+    path('all-listening-history/', all_listening_history,
+         name='all_listening_history')
 ]
