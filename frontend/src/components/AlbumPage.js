d80925061587fed98101335796d19a31b2e4720e
aidanwllmsn <ajwill1022@gmail.com>
2024-02-29 23:00:36 -0800

    Finished AlbumPage

--- frontend/src/components/AlbumPage.js
+++ frontend/src/components/AlbumPage.js
@@ -6,13 +6,13 @@ import Reviews from "./Reviews";
 
 const SongPage = ({ onPlay }) => {
   const { spotify_content_id } = useParams();
-  const [trackInfo, setTrackInfo] = useState(null);
+  const [albumInfo, setAlbumInfo] = useState(null);
 
   useEffect(() => {
     const callDjangoAPI = async () => {
       try {
         const response = await fetch(
-          "http://127.0.0.1:8000/reviews/get_track_info/",
+          "http://127.0.0.1:8000/reviews/get_album_info/",
           {
             method: "POST",
             headers: {
@@ -25,10 +25,10 @@ const SongPage = ({ onPlay }) => {
 
         if (response.ok) {
           const data = await response.json();
-          setTrackInfo(data); // Assuming data is the object with the track details
+          setAlbumInfo(data); // Assuming data is the object with the Album details
         } else {
           console.error("Failed to fetch song data");
-          setTrackInfo(null);
+          setAlbumInfo(null);
         }
       } catch (error) {
         console.error("There was an error!", error);
@@ -40,19 +40,37 @@ const SongPage = ({ onPlay }) => {
   }, [spotify_content_id]);
 
   return (
-    <div className="songpage">
-      <Grid container spacing={1}>
-        {/* Navbar grid item */}
-        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
-          {/* <Navbar /> */}
-        </Grid>
-        <Grid item xs={6} sm={6} md={5} lg={5} xl={5}>
-          <button onClick={() => onPlay(spotify_content_id, "album")}>
-            Play
-          </button>
-        </Grid>
+    <Grid container spacing={1}>
+      {/* Navbar grid item */}
+      <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+        {/* <Navbar /> */}
       </Grid>
-    </div>
+
+      {/* Songcard grid item */}
+      <Grid item xs={6} sm={6} md={5} lg={5} xl={5}>
+        {albumInfo ? (
+          <div align="center" className="songcard">
+            <img
+              src={albumInfo.images[0].url}
+              width="350px"
+              alt="Album Cover"
+              style={{ paddingTop: "20px" }}
+            />
+            <h1>{albumInfo.name}</h1>
+            <button onClick={() => onPlay(spotify_content_id, "album")}>
+              Play Song
+            </button>
+          </div>
+        ) : (
+          <h1>Loading Album info...</h1>
+        )}
+      </Grid>
+
+      {/* Reviews grid item */}
+      <Grid item xs={3} sm={3} md={5} lg={5} xl={5} align="center">
+        <Reviews spotifyContentId={spotify_content_id} />
+      </Grid>
+    </Grid>
   );
 };
 
